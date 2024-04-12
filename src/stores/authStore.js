772ae7c252e4eb559b8bf04f4dee26cb58177b2c
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-12 20:57:42 -0300

    changing code base

---
+++ src/stores/authStore.js
@@ -0,0 +1,26 @@
+import { writable } from 'svelte/store';
+import { createUserWithEmailAndPassword, signOut, sendPasswordResetEmail, updateEMail, updatePassword } from 'firebase/auth';
+import { auth } from '../lib/firebase/firebase.client';
+
+export const authStore = writable({
+    isLoading: true,
+    currentUser: null
+})
+
+export const authHandlers = {
+    signup: async (email, password) => {
+        await createUserWithEmailAndPassword(auth, email, password)
+    },
+    logout:  async () => {
+        await signOut(auth)
+    },
+    resetPassword: async (email) => {
+        await sendPasswordResetEmail(auth, email)
+    },
+    updateEmail: async (email) => {
+        await updateEMail(auth, email)
+    },
+    updatePassword: async (password) => {
+        await updatePassword(auth, password)
+    }
+}
