d053730c1957ddbd4d0e7e59375fb5d40f0e8005
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-12 21:08:03 -0300

    testnet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,19 @@
 <script>
-    import {onMount} from 'svelte'
-    import {auth} from '../lib/firebase/firebase.client'
-    
+    import {onMount} from 'svelte';
+    import {auth} from '../lib/firebase/firebase.client';
+    import {authStore} from '../stores/authStore';
+    import "../app.css";
+
     onMount(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             console.log(user)
+            authStore.update((curr) => {
+                return {...curr, isLoading: false, currentUser: user};
+            })
         })
     })
 </script>
-<slot />
+
+<main class="">
+    <slot />
+</main>
