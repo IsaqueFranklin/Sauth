1e9592f90b49c1b4aa503e7a7b9195184dcb709a
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-13 19:20:24 -0300

    sign up form design

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -1,3 +1,21 @@
-<div class="bg-black text-white">
-    <h1>HAHAHAHA!</h1>
+<script>
+    let register = false;
+</script>
+
+<div class="flex flex-col items-center justify-center my-auto">
+    <h1 class="text-3xl font-light mb-4">Sign up</h1>
+    <form class="flex flex-col">
+        <label class="mb-4">
+            <input class="px-2 py-2 border border-gray-800" type="text" placeholder="Email" />
+        </label>
+        <label class="mb-4">
+            <input class="px-2 py-2 border border-gray-800" type="password" placeholder="Password" />
+        </label>
+        {#if register}
+        <label class="mb-4">
+            <input class="px-2 py-2 border border-gray-800" type="password" placeholder="Confirm password" />
+        </label>
+        {/if}
+        <button class="px-1 py-2 border border-gray-800 hover:bg-gray-800 hover:text-white">Submit</button>
+    </form>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,6 @@
     })
 </script>
 
-<main class="">
+<main class="min-h-screen flex flex-col">
     <slot />
 </main>
